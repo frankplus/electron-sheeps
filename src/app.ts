15bb21e4bcb00536fa24bb1f9cd19943106b55d9
Francesco Magliocca <franciman12@gmail.com>
2017-10-04 11:58:50 +0200

    Add TranslationProject class with serialization/deserialization methods

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,15 @@
 
 const {ipcRenderer} = require('electron')
 
+let ts = require('./translation_project')
+
 ipcRenderer.on('extraction-finished', (event, exit_code) => {
     console.log(`Finished ${exit_code}`)
 });
 
+
+/*let proj = new ts.TranslationProject("/home/francesco/Desktop/Pushing.Daisies.S01.576p.BluRay.DD5.1.x264-HiSD/Pushing.Daisies.S01E01.576p.BluRay.DD5.1.x264-HiSD.mkv", "/home/francesco/output_peaks");
+
+ts.writeTranslationProjectToFile(proj, "./provaOutput");*/
+
+let proj = ts.loadTranslationProjectFile("./provaOutput");
---
+++ src/translation_project.ts
@@ -0,0 +1,31 @@
+import { readFileSync, writeFileSync } from 'fs';
+
+export class TranslationProject {
+    // The path of the multimedia file
+    // which contains the audio we want to sub
+    audioSourcePath: string;
+
+    // The path of the subtitle file we're going to edit
+    subtitleFilePath: string;
+
+    // The path of an optional subtitle file used as reference
+    referenceSubtitleFilePath?: string;
+
+    constructor(audioSourcePath: string, subtitleFilePath: string, referenceSubtitleFilePath?: string) {
+        this.audioSourcePath = audioSourcePath;
+        this.subtitleFilePath = subtitleFilePath;
+        this.referenceSubtitleFilePath = referenceSubtitleFilePath;
+    }
+}
+
+export function loadTranslationProjectFile(filePath: string): TranslationProject {
+    let object = JSON.parse(readFileSync(filePath, 'utf8'));
+
+    return new TranslationProject(object.audioSourcePath, object.subtitleFilePath, object.referenceSubtitleFilePath);
+}
+
+export function writeTranslationProjectToFile(project: TranslationProject, filePath: string) {
+    let json = JSON.stringify(project);
+
+    writeFileSync(filePath, json);
+}
