0cd5f306f854294aaf17b2870de9f0c5b667a0dd
Francesco Pham <pham.francesco@gmail.com>
2017-10-06 19:06:02 +0200

    try to implement switch of section

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,9 @@ const {ipcRenderer} = require('electron')
 let ts = require('./translation_project')
 
 //uncomment for example project
-//let proj = new ts.TranslationProject("/home/francesco/Desktop/Pushing.Daisies.S01.576p.BluRay.DD5.1.x264-HiSD/Pushing.Daisies.S01E01.576p.BluRay.DD5.1.x264-HiSD.mkv", "/home/francesco/output_peaks");
+let proj = new ts.TranslationProject("/home/francesco/Desktop/Pushing.Daisies.S01.576p.BluRay.DD5.1.x264-HiSD/Pushing.Daisies.S01E01.576p.BluRay.DD5.1.x264-HiSD.mkv", "/home/francesco/output_peaks");
 
+//ipc main process-renderers listeners
 ipcRenderer.on('open-project', (event, file) => {
     proj = ts.loadTranslationProjectFile(file);
     console.log(`project opened ${file}`)
@@ -18,3 +19,23 @@ ipcRenderer.on('save-project', (event, outputfile) => {
     ts.writeTranslationProjectToFile(proj, outputfile);
     console.log(`project saved`)
 });
+
+/*
+let form = document.getElementById("newProjectForm");
+form.addEventListener('submit', function createNewProject(ev) {
+    // Prevent <form> from sending a request, we're overriding this behavior here
+    ev.preventDefault();
+
+    console.log("switching section")
+
+    document.getElementById("newProjectSection").classList.remove('is-shown');
+    document.getElementById("container").classList.add('is-shown');
+});
+*/
+
+function submitproject(){
+    console.log("switching section")
+
+    document.getElementById("newProjectSection").classList.remove('is-shown');
+    document.getElementById("container").classList.add('is-shown');
+}
