3fd5cdfab039ac80104d24bbe542921807f34301
Francesco Pham <pham.francesco@gmail.com>
2017-10-06 22:11:47 +0200

    create a project object based on the newproject input data

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,23 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-const {ipcRenderer} = require('electron')
+const { ipcRenderer } = require('electron')
 
 let ts = require('./translation_project')
 
 //uncomment for example project
-let proj = new ts.TranslationProject("/home/francesco/Desktop/Pushing.Daisies.S01.576p.BluRay.DD5.1.x264-HiSD/Pushing.Daisies.S01E01.576p.BluRay.DD5.1.x264-HiSD.mkv", "/home/francesco/output_peaks");
+let proj;
 
 //ipc main process-renderers listeners
 ipcRenderer.on('open-project', (event, file) => {
     proj = ts.loadTranslationProjectFile(file);
-    console.log(`project opened ${file}`)
+    console.log(`project opened ${file}`);
+    switchToMainSection();
 });
 
 ipcRenderer.on('save-project', (event, outputfile) => {
     ts.writeTranslationProjectToFile(proj, outputfile);
-    console.log(`project saved`)
+    console.log(`project saved`);
 });
 
 let form = document.getElementById("newProjectForm");
@@ -25,8 +26,26 @@ form.addEventListener('submit', function createNewProject(ev) {
     // Prevent <form> from sending a request, we're overriding this behavior here
     ev.preventDefault();
 
-    console.log("switching section")
+    //create project using input data
+    let source:any = document.getElementById("audioSource");
+    let subfile:any = document.getElementById("subtitleFile");
+    let refsubfile:any = document.getElementById("referenceSubtitleFile");
 
+    let audiosourcepath = source.files[0].path;
+    let subfilepath = subfile.files[0].path;
+    let refsubfilepath = refsubfile.files[0].path;
+
+    console.log("source: " + audiosourcepath);
+    console.log("subfile: " + subfilepath);
+    console.log("refsubfile: " + refsubfilepath);
+
+    proj = new ts.TranslationProject(audiosourcepath, subfilepath, refsubfilepath);
+
+    switchToMainSection();
+});
+
+function switchToMainSection(){
+    //change view to the main program section
     document.getElementById("newProjectSection").classList.remove('is-shown');
     document.getElementById("mainSection").classList.add('is-shown');
-});
+}
