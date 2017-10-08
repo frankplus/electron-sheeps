9139a570e09d94283d23ca7639a671a0a242bba4
Francesco Magliocca <franciman12@gmail.com>
2017-10-08 18:14:57 +0200

    Create Application Manager class

--- src/app.ts
+++ src/app.ts
@@ -2,60 +2,66 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-const { ipcRenderer } = require('electron')
+const { ipcRenderer } = require('electron');
 
-let ts = require('./translation_project')
+let ts = require('./translation_project');
 
-//uncomment for example project
-let proj;
+class ApplicationManager {
+    project?: any;
 
-//ipc main process-renderers listeners
-ipcRenderer.on('open-project', (event, file) => {
-    proj = ts.loadTranslationProjectFile(file);
-    console.log(`project opened ${file}`);
-    switchToMainSection();
-});
+    constructor() {
+        this.project = undefined;
 
-ipcRenderer.on('save-project', (event, outputfile) => {
-    ts.writeTranslationProjectToFile(proj, outputfile);
-    console.log(`project saved`);
-});
+        // Listen to events that change the application state
+        ipcRenderer.on('open-project', (event, file) => {
+            this.project = ts.loadTranslationProjectFile(file);
+            this.startProject();
+        });
 
-ipcRenderer.on('new-project', (event, outputfile) => {
-    switchToNewProjectSection();
-});
+        ipcRenderer.on('save-project', (event, outputFile) => {
+            if(this.project) {
+                ts.writeTranslationProjectToFile(this.project, outputFile);
+            }
+        });
 
-let form = document.getElementById("newProjectForm");
-form.addEventListener('submit', function createNewProject(ev) {
-    // Prevent <form> from sending a request, we're overriding this behavior here
-    ev.preventDefault();
+        ipcRenderer.on('new-project', (event, outputFile) => {
+            this.createNewProject();
+        });
 
-    //create project using input data
-    let source:any = document.getElementById("audioSource");
-    let subfile:any = document.getElementById("subtitleFile");
-    let refsubfile:any = document.getElementById("referenceSubtitleFile");
+        document.getElementById("newProjectForm").addEventListener('submit', (ev) => {
+            // Prevent <form> from sending an HTTP request, we're overriding this behaviour here
+            ev.preventDefault();
 
-    let audiosourcepath = source.files[0].path;
-    let subfilepath = subfile.files[0].path;
-    let refsubfilepath = refsubfile.files[0].path;
+            //create project using input data
+            let source:any = document.getElementById("audioSource");
+            let subfile:any = document.getElementById("subtitleFile");
+            let refsubfile:any = document.getElementById("referenceSubtitleFile");
 
-    console.log("source: " + audiosourcepath);
-    console.log("subfile: " + subfilepath);
-    console.log("refsubfile: " + refsubfilepath);
+            let audiosourcepath = source.files[0].path;
+            let subfilepath = subfile.files[0].path;
+            let refsubfilepath = refsubfile.files[0] ? refsubfile.files[0].path : undefined;
 
-    proj = new ts.TranslationProject(audiosourcepath, subfilepath, refsubfilepath);
+            console.log("source: " + audiosourcepath);
+            console.log("subfile: " + subfilepath);
+            console.log("refsubfile: " + refsubfilepath);
 
-    switchToMainSection();
-});
+            this.project =  new ts.TranslationProject(audiosourcepath, subfilepath, refsubfilepath);
 
-function switchToMainSection(){
-    //change view to the main program section
-    document.getElementById("newProjectSection").classList.remove('is-shown');
-    document.getElementById("mainSection").classList.add('is-shown');
+            this.startProject();
+        });
+    }
+
+    startProject() {
+        //change view to the main program section
+        document.getElementById("newProjectSection").classList.remove('is-shown');
+        document.getElementById("mainSection").classList.add('is-shown');
+    }
+
+    createNewProject() {
+        //change view to the new project form section
+        document.getElementById("newProjectSection").classList.add('is-shown');
+        document.getElementById("mainSection").classList.remove('is-shown');
+    }
 }
 
-function switchToNewProjectSection(){
-    //change view to the new project form section
-    document.getElementById("newProjectSection").classList.add('is-shown');
-    document.getElementById("mainSection").classList.remove('is-shown');
-}
+let appManager = new ApplicationManager();
